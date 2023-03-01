a49f74bd1679f7daf18cbf87ace8365fef62904a
Iris <91870928+Nimphus@users.noreply.github.com>
2023-03-01 23:36:48 +0100

    Création du projet
    - Ajout de index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!-- Initialisation des variables -->
+<?php 
+
+$myName="Inès Romdhane";
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    
+    <title><?=$myName?> - Accueil</title>
+</head>
+<body>
+    
+</body>
+</html>
